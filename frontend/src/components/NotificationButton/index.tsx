377455c6603a0f4d15ab1825291d7b3c143d6c1b
matheus.bonnet.glo <matheus.bonnet@globalhitss.com.br>
2022-07-11 14:46:47 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import Vector from '../../assets/img/Vector.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={Vector} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
